2fe4ed6f756cd439394f8c05470353b3534ded17
Yeonggwang <immutable000@gmail.com>
2021-10-26 01:16:25 +0900

    설명 주석 추가

--- baekjoon/01_1158/1158.py
+++ baekjoon/01_1158/1158.py
@@ -18,9 +18,14 @@ current_index = k
 while len(answer) != n:
     # 남은 사람
     size = len(nums)
-    # 제거 할 사람
+    # 제거 할 사람  
+    # k-1 -> index 는 0부터 시작하므로
+    # 제거할 index 값이 배열의 size 보다 클 경우 size를 나눈값이 인덱스가된다
     target = current_index - 1 if size > current_index else ((current_index-1) % size)
+    
+    # 답변에
     answer.append(nums.pop(target))
+    # 다음에 제거될 사람 index
     current_index = target + k
 
 # 아래 처럼 출력하면 틀리는 거 같다..
