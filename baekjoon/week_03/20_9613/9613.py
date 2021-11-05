ec07fb21208b136ff0a1e1c563a9b10fcfa1d319
Yeonggwang <immutable000@gmail.com>
2021-11-05 23:00:39 +0900

    solve: 9613 GCD합

--- baekjoon/week_03/20_9613/9613.py
+++ baekjoon/week_03/20_9613/9613.py
@@ -1,23 +1,3 @@
----
-file: "9613.py"
-name: "GCD 합"
-src: "https://www.acmicpc.net/problem/9613"
-tags:
-  - 유클리드 호제법
-  - 수학
-  - 정수론
-done: true
-draft: false
-level: 8
-difficulty: "Silver III"
-date: 2021-11-02
----
-
-# GCD 합
-
-## 정답 코드
-
-```python
 import sys
 
 def input(): return sys.stdin.readline().rstrip()
@@ -29,7 +9,6 @@ def GCD(a, b):
 
 for _ in range(int(input())):
 
-    # 가장 앞 원소는 생략한다.
     nums = list(map(int, input().split()[1:]))
 
     nums_count = len(nums)
@@ -42,4 +21,3 @@ for _ in range(int(input())):
             answer += GCD(nums[i], nums[j])
 
     print(answer)
-```
